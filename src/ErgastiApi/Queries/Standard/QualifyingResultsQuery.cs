4f0fcf63fb2705fa37f1a9f0a5a102c3e86df724
Søren Kruse <krusenen@gmail.com>
2017-04-30 20:26:59 +0200

    Qualifying query changes

--- src/ErgastiApi/Queries/Standard/QualifyingResultsQuery.cs
+++ src/ErgastiApi/Queries/Standard/QualifyingResultsQuery.cs
@@ -3,6 +3,6 @@ namespace ErgastApi.Queries
     public class QualifyingResultsQuery : StandardQuery
     {
         [QueryTerminator, QueryMethod("qualifying")]
-        protected object Qualifying { get; }
+        public virtual int? QualifyingPosition { get; set; }
     }
 }
