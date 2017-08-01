24be3fef5833aa05f61d4843f9fa17e2b96fede0
Søren Kruse <krusenen@gmail.com>
2017-08-01 15:59:31 +0200

    Add constant circuit ids

---
+++ src/ErgastiApi/Ids/CircuitIds.cs
@@ -0,0 +1,71 @@
+﻿namespace ErgastApi.Ids
+{
+    public static class CircuitIds
+    {
+        public const string Australia = AlbertPark;
+        public const string Melbourne = AlbertPark;
+        public const string AlbertPark = "albert_park";
+
+        public const string Italy = Monza;
+        public const string Monza = "monza";
+
+        public const string Hockenheim = "hockenheimring";
+        public const string Nurburgring = "nurburgring";
+
+        public const string Austria = RedBullRing;
+        public const string Spielburg = RedBullRing;
+        public const string RedBullRing = "red_bull_ring";
+
+        public const string Bahrain = "bahrain";
+
+        public const string USA = CircuitOfTheAmericas;
+        public const string Austin = CircuitOfTheAmericas;
+        public const string COTA = CircuitOfTheAmericas;
+        public const string CircuitOfTheAmericas = "americas";
+
+        public const string Spain = Catalunya;
+        public const string Catalunya = "catalunya";
+
+        public const string Hungary = Hungaroring;
+        public const string Budapest = Hungaroring;
+        public const string Hungaroring = "hungaroring";
+
+        public const string Brazil = Interlagos;
+        public const string SaoPaulo = Interlagos;
+        public const string Interlagos = "interlagos";
+
+        public const string Singapore = MarinaBay;
+        public const string MarinaBay = "marina_bay";
+
+        public const string MonteCarlo = Monaco;
+        public const string Monaco = "monaco";
+
+        public const string China = Shanghai;
+        public const string Shanghai = "shanghai";
+
+        public const string Belgium = Spa;
+        public const string SpaFrancorchamps = Spa;
+        public const string Spa = "spa";
+
+        public const string Russia = Sochi;
+        public const string Sochi = "sochi";
+
+        public const string Japan = Suzuka;
+        public const string Suzuka = "suzuka";
+
+        public const string Canada = CircuitGillesVilleneuve;
+        public const string Montreal = CircuitGillesVilleneuve;
+        public const string CircuitGillesVilleneuve = "villeneuve";
+
+        public const string UK = Silverstone;
+        public const string Britain = Silverstone;
+        public const string Silverstone = "silverstone";
+
+        public const string Malaysia = Sepang;
+        public const string Sepang = "sepang";
+
+        public const string UAE = YasMarina;
+        public const string AbuDhabi = YasMarina;
+        public const string YasMarina = "yas_marina";
+    }
+}
