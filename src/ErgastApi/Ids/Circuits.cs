d5cda09a7322da48338ffb8b780b62dc13020a47
Søren Kruse <krusenen@gmail.com>
2017-08-03 10:35:47 +0200

    Added some more circuit constants

--- src/ErgastApi/Ids/Circuits.cs
+++ src/ErgastApi/Ids/Circuits.cs
@@ -6,71 +6,145 @@
     public static class Circuits
     {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+        /// <summary>Albert Park Grand Prix Circuit</summary>
         public const string Australia = AlbertPark;
+        /// <summary>Albert Park Grand Prix Circuit</summary>
         public const string Melbourne = AlbertPark;
+        /// <summary>Albert Park Grand Prix Circuit</summary>
         public const string AlbertPark = "albert_park";
 
+        /// <summary>Autodromo Nazionale di Monza</summary>
         public const string Italy = Monza;
+        /// <summary>Autodromo Nazionale di Monza</summary>
         public const string Monza = "monza";
 
         public const string Hockenheim = "hockenheimring";
         public const string Nurburgring = "nurburgring";
 
+        /// <summary>Red Bull Ring</summary>
         public const string Austria = RedBullRing;
+        /// <summary>Red Bull Ring</summary>
         public const string Spielburg = RedBullRing;
+        /// <summary>Red Bull Ring</summary>
         public const string RedBullRing = "red_bull_ring";
 
+        /// <summary>Bahrain International Circuit</summary>
         public const string Bahrain = "bahrain";
 
-        public const string USA = CircuitOfTheAmericas;
+        /// <summary>Baku City Circuit</summary>
+        public const string Azerbaijan = Baku;
+        /// <summary>Baku City Circuit</summary>
+        public const string Baku = "BAK";
+
+        /// <summary>Circuit of the Americas</summary>
         public const string Austin = CircuitOfTheAmericas;
+        /// <summary>Circuit of the Americas</summary>
         public const string COTA = CircuitOfTheAmericas;
+        /// <summary>Circuit of the Americas</summary>
         public const string CircuitOfTheAmericas = "americas";
 
+        public const string Mexico = AutodromoHermanosRodriguez;
+        public const string AutodromoHermanosRodriguez = "rodriguez";
+
+        /// <summary>Circuit de Barcelona-Catalunya</summary>
         public const string Spain = Catalunya;
+        /// <summary>Circuit de Barcelona-Catalunya</summary>
+        public const string Barcelona = Catalunya;
+        /// <summary>Circuit de Barcelona-Catalunya</summary>
         public const string Catalunya = "catalunya";
 
+        /// <summary>Hungaroring</summary>
         public const string Hungary = Hungaroring;
+        /// <summary>Hungaroring</summary>
         public const string Budapest = Hungaroring;
+        /// <summary>Hungaroring</summary>
         public const string Hungaroring = "hungaroring";
 
+        /// <summary>Autódromo José Carlos Pace (Interlagos)</summary>
         public const string Brazil = Interlagos;
+        /// <summary>Autódromo José Carlos Pace (Interlagos)</summary>
         public const string SaoPaulo = Interlagos;
+        /// <summary>Autódromo José Carlos Pace (Interlagos)</summary>
         public const string Interlagos = "interlagos";
 
+        /// <summary>Marina Bay Street Circuit</summary>
         public const string Singapore = MarinaBay;
+        /// <summary>Marina Bay Street Circuit</summary>
         public const string MarinaBay = "marina_bay";
 
+        /// <summary>Circuit de Monaco</summary>
         public const string MonteCarlo = Monaco;
+        /// <summary>Circuit de Monaco</summary>
         public const string Monaco = "monaco";
 
+        /// <summary>Shanghai International Circuit</summary>
         public const string China = Shanghai;
+        /// <summary>Shanghai International Circuit</summary>
         public const string Shanghai = "shanghai";
 
+        /// <summary>Circuit de Spa-Francorchamps</summary>
         public const string Belgium = Spa;
+        /// <summary>Circuit de Spa-Francorchamps</summary>
         public const string SpaFrancorchamps = Spa;
+        /// <summary>Circuit de Spa-Francorchamps</summary>
         public const string Spa = "spa";
 
+        /// <summary>Sochi Autodrom</summary>
         public const string Russia = Sochi;
+        /// <summary>Sochi Autodrom</summary>
         public const string Sochi = "sochi";
 
+        /// <summary>Suzuka Circuit</summary>
         public const string Japan = Suzuka;
+        /// <summary>Suzuka Circuit</summary>
         public const string Suzuka = "suzuka";
 
+        /// <summary>Circuit Gilles Villeneuve</summary>
         public const string Canada = CircuitGillesVilleneuve;
+        /// <summary>Circuit Gilles Villeneuve</summary>
         public const string Montreal = CircuitGillesVilleneuve;
+        /// <summary>Circuit Gilles Villeneuve</summary>
         public const string CircuitGillesVilleneuve = "villeneuve";
 
+        /// <summary>Silverstone Circuit</summary>
         public const string UK = Silverstone;
+        /// <summary>Silverstone Circuit</summary>
         public const string Britain = Silverstone;
+        /// <summary>Silverstone Circuit</summary>
         public const string Silverstone = "silverstone";
 
+        /// <summary>Sepang International Circuit</summary>
         public const string Malaysia = Sepang;
+        /// <summary>Sepang International Circuit</summary>
+        public const string KualaLumpur = Sepang;
+        /// <summary>Sepang International Circuit</summary>
         public const string Sepang = "sepang";
 
+        /// <summary>Yas Marina Circuit</summary>
         public const string UAE = YasMarina;
+        /// <summary>Yas Marina Circuit</summary>
         public const string AbuDhabi = YasMarina;
+        /// <summary>Yas Marina Circuit</summary>
         public const string YasMarina = "yas_marina";
+
+        /// <summary>Autodromo Enzo e Dino Ferrari</summary>
+        public const string Imola = "imola";
+
+        /// <summary>Indianapolis Motor Speedway</summary>
+        public const string Indianapolis = "indianapolis";
+
+        /// <summary>Circuit de Nevers Magny-Cours</summary>
+        public const string MagnyCours = "magny_cours";
+
+        /// <summary>Circuit Paul Ricard</summary>
+        public const string PaulRicard = "ricard";
+
+        /// <summary>Adelaide Street Circuit</summary>
+        public const string Adelaide = "adelaide";
+
+        /// <summary>Circuito de Jerez</summary>
+        public const string Jerez = "jerez";
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
