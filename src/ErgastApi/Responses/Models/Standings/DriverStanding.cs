4c63de7d2f669c5e2054934b51e64eb604924d9a
Søren Kruse <krusenen@gmail.com>
2017-09-10 13:09:17 +0200

    Add constructor info to DriverStanding
    +semver:minor

--- src/ErgastApi/Responses/Models/Standings/DriverStanding.cs
+++ src/ErgastApi/Responses/Models/Standings/DriverStanding.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
 namespace ErgastApi.Responses.Models.Standings
 {
     public class DriverStanding : Standing
     {
         public Driver Driver { get; set; }
+
+        /// <summary>
+        /// The latest constructor which the driver has driven for.
+        /// </summary>
+        public Constructor Constructor => AllConstructors.LastOrDefault();
+
+        /// <summary>
+        /// A list of all the constructors the driver has driven for up to this point in the season.
+        /// </summary>
+        [JsonProperty("Constructors")]
+        public IList<Constructor> AllConstructors { get; set; }
     }
 }
